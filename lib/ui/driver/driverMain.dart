8e681816ad5f8c2e0a5d03d42b48cb3b8f71f175
mayuresh-surve <me.mayuresh.99@gmail.com>
2021-03-26 22:08:58 +0530

    assets added

--- lib/ui/driver/driverMain.dart
+++ lib/ui/driver/driverMain.dart
@@ -4,8 +4,24 @@ class DriverMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Driver Page")
+      appBar: AppBar(title: Text("Driver Page")),
+      body: ListView(
+        children: [
+          Card(
+            elevation: 5.0,
+            child: Row(
+              children: [
+                Text(
+                  'Honda sevice center',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
