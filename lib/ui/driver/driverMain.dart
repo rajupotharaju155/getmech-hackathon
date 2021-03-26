35e0a5feead8554a48b8b887d7a0ba35f13b1edc
mayuresh-surve <me.mayuresh.99@gmail.com>
2021-03-26 23:05:11 +0530

    slash added

--- lib/ui/driver/driverMain.dart
+++ lib/ui/driver/driverMain.dart
@@ -10,14 +10,46 @@ class DriverMainPage extends StatelessWidget {
           Card(
             elevation: 5.0,
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Honda sevice center',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/garage_1.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          'Honda sevice center',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5.0),
+                        child: Text(
+                          'Adding some random street address here!',
+                          style: TextStyle(fontSize: 14),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
